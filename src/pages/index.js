a51ecd489e669924def0be5c07a301ea43cabc2c
westc0ld <oso0035@naver.com>
2024-05-11 12:01:55 +0900

    2차

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Layout from "../components/layout";
-import InteractiveQueryApp from "../components/index.body"
-
+import InteractiveQueryApp from "../components/index.body";
 
 const inter = Inter({ subsets: ["latin"] });
 
