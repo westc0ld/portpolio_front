b5f2011082bf2c61a1cee2bf483e40a549a63450
westc0ld <oso0035@naver.com>
2024-06-15 01:29:43 +0900

    9차

--- src/components/index.body.js
+++ src/components/index.body.js
@@ -23,7 +23,7 @@ const ChatGPT = () => {
     const sendMessage = async () => {
         setIsLoading(true); // 메시지 전송 시작 시 로딩 상태를 true로 설정합니다.
         try {
-            const response = await axios.post('http://13.58.170.184/sendMessage', {
+            const response = await axios.post('https://api.westcold0035.com', {
                 user_input: inputValue
             });
             setResponseMessage(response.data.description);
