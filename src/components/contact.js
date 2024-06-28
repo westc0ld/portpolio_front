3c348dc778a7df24bf0c717a3018524185c99850
westc0ld <oso0035@naver.com>
2024-06-29 02:46:54 +0900

    contact1차

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,10 +3,12 @@
 import React from 'react';
 import ContactForm from './contactform';
 
+
 const ContactPage = () => {
   return (
     <div className="contact_container">
       <ContactForm />
+
     </div>
   );
 };
--- src/components/contactform.js
+++ src/components/contactform.js
@@ -1,26 +1,25 @@
-// ContactForm.js
-
 import React from 'react';
-
 
 const ContactForm = () => {
   return (
-    <div className='contactform'>
-      <div className="head">Contact</div>
-      <div className="input_container">
-        <div className="mb-1">
-          <label htmlFor="name" className="block text-gray-700">Name</label>
-          <input type="text" id="name" name="name" className="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
+    <div className='contact_container'>
+      <div className="contactform">
+        <div className="head">Contact</div>
+        <div className="input_container">
+          <div className="contact_name">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div className="contact_email">
+            <label htmlFor="email">Email Address</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="contact_message">
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message" rows="4"></textarea>
+          </div>
+          <button type="submit" className="contact_send">Send</button>
         </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700">Email Address</label>
-          <input type="email" id="email" name="email" className="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="message" className="block text-gray-700">Message</label>
-          <textarea id="message" name="message" rows="4" className="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"></textarea>
-        </div>
-        <button type="submit" className="bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600">Send</button>
       </div>
     </div>
   );
